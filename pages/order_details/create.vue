2b99e82a576c6b76c5876de800309b1556080eca
ViVu1002 <nguyenthila100221@gmail.com>
2019-11-06 18:05:06 +0700

    text

---
+++ pages/order_details/create.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input type="integer"  v-model="form.orderId" placeholder="Order_id...">
+        <input type="integer" v-model="form.price" placeholder="Price...">
+        <b-button @click="handleCreate" variant="info">Create</b-button>
+    </div>
+</template>
+<script>
+export default {
+    data: function(){
+        return {
+            form : {
+                orderId :'',
+                price : '',
+            }
+        }
+    },
+    methods : {
+        handleCreate: function(){
+            let self = this
+            this.$axios.post('/order_details', this.form)
+            .then(function(res){
+                console.log(res)
+                self.$router.push('/order_details')
+            })
+        }
+    }
+}
+</script>
---
+++ pages/order_details/index.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <b-button @click="$router.push('/order_details/create')" variant="info">Create</b-button>
+        <input type="search" placeholder="Search start.." v-model="search">
+        <b-button @click="handleSearch" variant="info">Search</b-button>
+        <table>
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Order_id</th>
+                    <th>Price</th>
+                    <th>Action</th> 
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(order_detail , index) in order_details" :key="index">
+                    <td>{{index+1}}</td>
+                    <td>{{order_detail.orderId}}</td>
+                    <td>{{order_detail.price}}</td>
+                    <td>
+                        <b-button @click="$router.push('/order_details/detail/'+order_detail.id)">Detail</b-button>
+                        <b-button @click="$router.push('/order_details/edit/'+order_detail.id)" variant="success">Edit</b-button>
+                        <b-button @click="handleDelete(order_detail.id)" variant="danger">Delete</b-button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <b-pagination
+            v-model="pagination.currentPage"
+            :total-rows="pagination.total"
+            :per-page="pagination.perPage"
+            aria-controls="my-table"
+            @change="handleChangePage"
+        ></b-pagination>
+    </div>
+</template>
+<script>
+export default {
+    mounted : function(){
+        this.getOrder_details()
+    },
+    data : function(){
+        return {
+            order_details : {
+                 index : '',
+                 orderId :'',
+                 price: '',
+            },
+            search :'',
+            totalResult : 0,
+            pagination: {
+                currentPage : 1,
+                perPage: 10,
+            }
+        }
+    },
+    methods : {
+        getOrder_details : function(){
+            let self = this
+            this.$axios.get('/order_details?search='+this.search+'&currentPage='+this.pagination.currentPage+'&perPage='+this.pagination.perPage)
+            .then(function(res){
+                console.log(res)
+                self.order_details = res.data.data
+                self.pagination = res.data.pagination
+            })
+        },
+         handleDelete: function(id){
+            let self = this
+            this.$axios.delete('/order_details/'+id)
+            .then(function(res){
+                console.log(res)
+                self.getOrder_details()
+            })
+        },
+        handleSearch : function(){
+            this.getOrder_details()
+        },
+         handleChangePage: function(page){
+            this.pagination.currentPage = page
+            this.getOrder_details()
+        }
+    }
+}
+</script>
