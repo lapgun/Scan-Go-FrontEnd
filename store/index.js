00935143621751ba685b965e2b63ec757c040b3b
lapgun <lapgun06@gmail.com>
2019-11-06 19:07:01 +0700

    login

---
+++ store/index.js
@@ -0,0 +1,33 @@
+const cookieparser = process.server ? require('cookieparser') : undefined
+export const state = () => {
+  return {
+    globalText: 'No thing',
+    token: '',
+    user : {}
+  }
+};
+export const mutations = {
+  setToken(state, token) {
+    state.token = token
+  },
+  setUser(state,user){
+    state.user =user
+  }
+};
+export const actions = {
+  nuxtServerInit({commit}, {req}) {
+    let token = null;
+    let user = {};
+    if (req.headers.cookie) {
+      const parsed = cookieparser.parse(req.headers.cookie);
+      token = parsed.token;
+      commit('setToken',token)
+    }
+    if (req.headers.cookie){
+      const parsed = cookieparser.parse(req.headers.cookie);
+      user = parsed.user;
+      commit('setUser', user)
+    }
+
+  }
+};
