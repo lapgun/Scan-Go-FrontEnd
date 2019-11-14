1ed6867731e2d1087ef231a61355f3dbae4c2578
ViVu1002 <nguyenthila100221@gmail.com>
2019-11-14 17:57:55 +0700

    text

--- components/shopHeader.vue
+++ components/shopHeader.vue
@@ -12,17 +12,6 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-6">
-						<div class="social-icons pull-right">
-							<ul class="nav navbar-nav">
-								<li><a href="#"><i class="fab fa-facebook"></i></a></li>
-								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
-								<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-								<li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-								<li><a href="#"><i class="fab fa-google-plus"></i></a></li>
-							</ul>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div><!--/header_top-->
@@ -38,11 +27,10 @@
 					<div class="col-sm-8">
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
-								<li><a href="#" style="margin-left:-270px"><i class="fa fa-user"></i> Account</a></li>
-								<li><a href="#" style="margin-left:-200px; margin-top:-20px" ><i class="fa fa-star"></i> Wishlist</a></li>
-								<li><a href="#" style="margin-left: -130px; margin-top:-20px"><i class="fa fa-crosshairs"></i> Checkout</a></li>
-								<li><a href="#" style="margin-left:-60px; margin-top:-20px"><i class="fa fa-shopping-cart"></i> Cart</a></li>
-								<li><a href="#" style=" margin-top:-20px"><i class="fa fa-lock"></i> Login</a></li>
+								<li><a @click="$router.push('/user/detail/'+user.id)" style="margin-left:-220px"><i class="fa fa-user"></i> Account</a></li>
+								<li><a @click="$router.push('/shop/checkout/'+user.id)" style="margin-left: -150px; margin-top:-20px"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+								<li><a @click="$router.push('/user/register')" style="margin-left:-70px; margin-top:-20px"><i class="fa fa-shopping-cart"></i> Register</a></li>
+								<li><a @click="$router.push('/login')" style=" margin-top:-20px"><i class="fa fa-lock"></i> Login</a></li>
 							</ul>
 						</div>
 					</div>
@@ -54,34 +42,20 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-9">
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-								<span class="sr-only">Toggle navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-						</div>
 						<div class="mainmenu pull-left">
 							<ul class="nav navbar-nav collapse navbar-collapse">
-								<li><a href="index.html" class="active" style="margin-top:-2px">Home</a></li>
+								<li><a href="#" class="active" style="margin-top:-2px">Home</a></li>
 								<li class="dropdown"><a href="#" style="margin-top:-10px">Shop<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
-                                        <li><a href="shop.html">Products</a></li>
-										<li><a href="product-details.html">Product Details</a></li> 
-										<li><a href="checkout.html">Checkout</a></li> 
-										<li><a href="cart.html">Cart</a></li> 
-										<li><a href="login.html">Login</a></li> 
+                                        <li><a @click="$router.push('/shop/product')">Products</a></li>
+										<li><a @click="$router.push('/shop/checkout/'+user.id)">Checkout</a></li> 
+										<li><a @click="$router.push('/shop/cart')">Cart</a></li> 
+										<li><a @click="$router.push('/login')">Login</a></li> 
                                     </ul>
                                 </li> 
-								<li class="dropdown"><a href="#" style="margin-top:-10px">Blog<i class="fa fa-angle-down"></i></a>
-                                    <ul role="menu" class="sub-menu">
-                                        <li><a href="blog.html">Blog List</a></li>
-										<li><a href="blog-single.html">Blog Single</a></li>
-                                    </ul>
+								<li><a @click="$router.push('/shop/blog')">Blog<i class="fa fa-angle-down"></i></a>
                                 </li> 
-								<li><a href="404.html">404</a></li>
-								<li><a href="contact-us.html">Contact</a></li>
+								<li><a @click="$router.push('/shop/contact')">Contact</a></li>
 							</ul>
 						</div>
 					</div>
