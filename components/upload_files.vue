85300cce0d32d9fb6a4c4709cebed3e24537e4ea
lapgun <lapgun06@gmail.com>
2019-11-14 20:52:21 +0700

    update

--- components/upload_files.vue
+++ components/upload_files.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="container">
-    <input type="file" id="files" ref="files" multiple v-on:change="submitFiles()"/>
-  </div>
+    <div class="container">
+        <input type="file" id="files" ref="files" multiple v-on:change="handleFilesUpload()"/>
+        <div class="large-12 medium-12 small-12 cell">
+            <div v-for="(file, key) in files" class="file-listing">{{ file.name }} <span class="remove-file"
+                                                                                         v-on:click="removeFile( key )">Remove</span>
+            </div>
+        </div>
+        <br>
+        <div class="large-12 medium-12 small-12 cell">
+            <button v-on:click="addFiles()">Add Files</button>
+        </div>
+        <br>
+        <div class="large-12 medium-12 small-12 cell">
+            <button v-on:click="submitFiles()">Submit</button>
+        </div>
+    </div>
+
 </template>
 <script>
     export default {
         data() {
             return {
-                files : []
+                files: []
             }
         },
         methods: {
+            addFiles() {
+                this.$refs.files.click();
+            },
             submitFiles() {
-                this.handleFilesUpload();
                 let formData = new FormData();
                 for (let i = 0; i < this.files.length; i++) {
                     let file = this.files[i];
@@ -21,25 +37,42 @@
                 }
                 let self = this;
                 console.log(formData);
-                this.$axios.post('/gallery_products/upload', formData,  {
+                this.$axios.post('/gallery_products/upload', formData, {
                     headers: {
                         'Content-Type': 'multipart/form-data'
                     }
-                }).then(function(res){
+                }).then(function (res) {
                     console.log(res);
-                   self.files ='';
+                    self.files = '';
                 })
 
             },
             handleFilesUpload() {
                 let uploadedFiles = this.$refs.files.files;
-                for( let i = 0; i < uploadedFiles.length; i++ ){
-                    this.files.push( uploadedFiles[i] );
+                for (let i = 0; i < uploadedFiles.length; i++) {
+                    this.files.push(uploadedFiles[i]);
                 }
             },
+            removeFile(key) {
+                this.files.splice(key, 1);
+            }
         }
 
     }
 </script>
 <style scoped>
+    input[type="file"] {
+        position: absolute;
+        top: -500px;
+    }
+
+    div.file-listing {
+        width: 200px;
+    }
+
+    span.remove-file {
+        color: red;
+        cursor: pointer;
+        float: right;
+    }
 </style>
